16dbf7c96a473170898fdbcd6f74398d601eed38
Benyla <s224194@dtu.dk>
2025-04-29 13:09:42 +0200

    Update VAE.py

--- src/models/VAE.py
+++ src/models/VAE.py
@@ -8,15 +8,15 @@ class Discriminator(nn.Module):
         super().__init__()
         self.layer1 = nn.Sequential(
             nn.Conv2d(in_channels, 32, kernel_size=4, stride=2, padding=1),
-            nn.LeakyReLU(0.2, inplace=True)
+            nn.LeakyReLU(0.2)
         )
         self.layer2 = nn.Sequential(
             nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),
-            nn.LeakyReLU(0.2, inplace=True)
+            nn.LeakyReLU(0.2)
         )
         self.layer3 = nn.Sequential(
             nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
-            nn.LeakyReLU(0.2, inplace=True)
+            nn.LeakyReLU(0.2)
         )
         self.logit = nn.Conv2d(128, 1, 8)
 
