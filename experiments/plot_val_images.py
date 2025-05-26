fd04aa4f67c60658ac0ddf1a6557a2db6c2eec6f
Benyla <s224194@dtu.dk>
2025-05-26 15:34:42 +0200

    Update plot_val_images.py

--- experiments/plot_val_images.py
+++ experiments/plot_val_images.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
-import os
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
+
 import argparse
 import pandas as pd
 import numpy as np
